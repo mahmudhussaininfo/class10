7a838695f2422b9ce3d07114f8da675881775243
Mahmud <77590197+mahmudhussaininfo@users.noreply.github.com>
2022-05-18 21:49:35 +0600

    upload my assignment

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// Regular Expression 
+
+// Create an email pattern
+
+// let mail = "mahmud-hussain10@yahoo.com";
+
+// let pattern = /^[a-z0-9-_.]*@[a-z]{2,8}\.[a-z]{2,7}$/;
+
+// console.log(pattern.test(mail));
+
+
+// Create a  username pattern
+
+// let user = "KUDDusuddin500";
+
+// let pat = /^[a-zA-Z0-9]*$/;
+
+// console.log(pat.test(user));
+
+// # Create a Bangladeshi phone number pattern
+
+
+// let number = "+8801776757650";
+
+// let pattern = /^(\+8801|01)[0-9]{9}$/;
+
+// console.log(pattern.test(number));
+
+
+// # Create a password pattern 
+
+// let pass = "Mamuf10$%^";
+
+// let pa = /^[a-zA-Z0-9!@#$%^&*]{6,12}$/;
+
+// console.log(pa.test(pass));
+
+// # Create a zipcode pattern  
+
+// let zip = "0821";
+
+// let zi = /^[0-9]{3,7}$/;
+
+// console.log(zi.test(zip));
